a75840a2aad21a0fdcff97a0ff65cfcc7b3cd3f2
Andrewharber <harber.andrew@ymail.com>
2022-07-01 13:25:35 -0400

    commit tree 21f55eda84263b54ff258902779ead5734667057

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const name = ["Guadalupe", "Oli", "Aki" ];
+let thankYou = []
+
+function writeCards(name, surprise) {
+    let thankYou = []
+    for (let i = 0; i < name.length; i++ ) {
+    
+    thankYou.push(`Thank you, ${name[i]}, for the wonderful ${surprise} gift!`);
+    debugger;
+    }
+    return thankYou;
+}
+writeCards(["Guadalupe", "Oli", "Aki"], "surprise");
+
+
+function countDown(integer) {
+    let i = 15;
+    while (integer >= 0) {
+      console.log(integer--);
+}
+
+    return integer;
+};
